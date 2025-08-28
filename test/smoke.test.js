dc8c9dc7b73481bf50131a6decaf5319b4013243
jtweilan <jace.weiland922@gmail.com>
2025-08-28 18:30:31 -0400

    Fix PLSS layer URL

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 
 const html = fs.readFileSync('index.html', 'utf8');
 
-if (!html.includes('PLSS Sections')) {
-  console.error('PLSS Sections layer not found');
+const layerUrl = 'https://gis.blm.gov/arcgis/rest/services/BLM_National_Data/PLSSCadNSDI/MapServer/16';
+
+if (!html.includes(layerUrl)) {
+  console.error('PLSS Sections layer not found or URL incorrect');
   process.exit(1);
 }
 
