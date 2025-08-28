ed8c66d0a51ccb2d44a0e5c5a199da4835865901
jtweilan <jace.weiland922@gmail.com>
2025-08-28 18:36:41 -0400

    Use stable PLSS sections FeatureLayer

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const html = fs.readFileSync('index.html', 'utf8');
 
-const layerUrl = 'https://gis.blm.gov/arcgis/rest/services/BLM_National_Data/PLSSCadNSDI/MapServer/16';
+const layerUrl = 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_PLSS_Sections/FeatureServer/0';
 
 if (!html.includes(layerUrl)) {
   console.error('PLSS Sections layer not found or URL incorrect');
